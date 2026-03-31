43b0df1f2878906060c9914d15b69902d81b2541
bbaymistery <elgun.ezmemmedov@mail.ru>
2026-03-31 18:14:39 +0400

    admin sifaris yontemi order backen d islemleri

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -6,12 +6,15 @@ import Customers from "../../components/customers/Customers";
 import MenuWrapper from "../../components/product/MenuWrapper";
 import Reservation from "../../components/Reservation";
 
-const Index = ({ categoryList }) => {
+const Index = ({ categoryList, productList }) => {
+    console.log("sda");
+    console.log(categoryList, productList);
+    
     return (
         <>
             <Carousel />
             <Campaigns />
-            <MenuWrapper categoryList={categoryList} />
+            <MenuWrapper categoryList={categoryList} productList={productList} />
             <About />
             <Reservation />
             <Customers />
@@ -20,12 +23,3 @@ const Index = ({ categoryList }) => {
 };
 
 export default Index;
-
-export const getServerSideProps = async () => {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories`);
-    return {
-        props: {
-            categoryList: res.data ? res.data : [],
-        },
-    };
-};
--- pages/index.jsx
+++ pages/index.jsx
@@ -17,15 +17,23 @@ export default function Index({ categoryList, productList }) {
           rel="stylesheet"
         />
       </Head>
-      <Home categoryList={categoryList} />
+      <Home categoryList={categoryList} productList={productList} />
     </div>
   );
 }
+
 export const getServerSideProps = async () => {
-  const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories`);
-  return {
-    props: {
-      categoryList: res.data ? res.data : [],
-    },
-  };
+    const category = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/categories`
+    );
+    const product = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/products`
+    );
+
+    return {
+        props: {
+            categoryList: category.data ? category.data : [],
+            productList: product.data ? product.data : [],
+        },
+    };
 };
